be67c82ca7f3ff062215d8ea83322a5be6806a5d
Geniorama <geniorama.info@gmail.com>
2019-02-18 19:26:09 -0500

    Scroll horizontal

--- js/main.js
+++ js/main.js
@@ -10,5 +10,35 @@ $(document).ready(function(){
 */
 
 
-new WOW().init();
+//new WOW().init();
+
+$('#pagepiling').pagepiling({
+    menu: null,
+    direction: 'horizontal',
+    verticalCentered: true,
+    sectionsColor: [],
+    anchors: [],
+    scrollingSpeed: 700,
+    easing: 'swing',
+    loopBottom: false,
+    loopTop: false,
+    css3: true,
+    navigation: {
+        'textColor': '#ccc',
+        'bulletsColor': '#000',
+        'position': 'right',
+        'tooltips': ['Intro', 'About', 'section3', 'section4']
+    },
+       normalScrollElements: null,
+    normalScrollElementTouchThreshold: 5,
+    touchSensitivity: 5,
+    keyboardScrolling: true,
+    sectionSelector: '.section',
+    animateAnchor: false,
+
+    //events
+    onLeave: function(index, nextIndex, direction){},
+    afterLoad: function(anchorLink, index){},
+    afterRender: function(){},
+});
 });
